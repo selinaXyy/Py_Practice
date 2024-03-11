bf2318b2315a8ec2f21bf2a77de3ad490cd2322e
selinaXyy <158121120+selinaXyy@users.noreply.github.com>
2024-03-10 19:14:42 -0700

    Update paddle.py

--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -1,9 +1,5 @@
 from turtle import Turtle
 
-STEP_SIZE = 20
-Y_MAX = 240
-Y_MIN = -240
-
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
@@ -16,10 +12,10 @@ class Paddle(Turtle):
 
     def go_up(self):
         y = self.ycor()
-        if y <= Y_MAX:
-            self.sety(y + STEP_SIZE)
+        if y <= 240:
+            self.sety(y + 20)
 
     def go_down(self):
         y = self.ycor()
-        if y >= Y_MIN:
-            self.sety(y - STEP_SIZE)
+        if y >= -240:
+            self.sety(y - 20)
