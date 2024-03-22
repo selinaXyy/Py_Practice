13d094247114138efd6915a798fcde9b06f34812
selinaXyy <yiyangxue.xyy@gmail.com>
2024-03-22 12:10:52 -0700

    Update main.py, data.txt
    Fixed some bugs

--- password_manager(tkinter)/main.py
+++ password_manager(tkinter)/main.py
@@ -6,6 +6,9 @@ DEFAULT_EMAIL = "selinaxue2@gmail.com"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #referenced from the password generator project
 def generate_password():
+    #clears previous entry
+    entry_password.delete(0, END)
+
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -17,7 +20,7 @@ def generate_password():
 
     password_list = letters_list + symbols_list + numbers_list
 
-    random.shuffle(password_list)
+    shuffle(password_list)
 
     password = "".join(password_list) #used on strings
 
